9dd6c26abed189fa3f43ebb304ed13686a19e5d2
Kazuya-Sakashita <kz0508@gmail.com>
2022-05-21 06:09:57 +0900

    プロフィールページ編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, # この行にカンマを追加
+    controllers: { registrations: 'registrations' } # この行を追加
+
   root 'top#index'
 
   resources :users, only: [:show]
 end
+
